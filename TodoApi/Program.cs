8ba3cadb869d454be54ddcfaad3372bc2be99457
Raul Naupari <raulnq@gmail.com>
2023-12-28 08:27:39 -0500

    razor components

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,23 +1,11 @@
-using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http.HttpResults;
 using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents();
+
 var app = builder.Build();
-
-app.MapGet("/hello-world", () => new HtmlResult(@"<!doctype html>
-<html>
-    <head>
-        <meta charset=""UTF-8"">
-        <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
-        <script src=""https://unpkg.com/htmx.org@1.9.6"" integrity=""sha384-FhXw7b6AlE/jyjlZH5iHa/tTe9EpJ1Y55RjcgPbjeWMskSxZt1v9qkxLJWNJaGni"" crossorigin=""anonymous""></script>
-    </head>
-    <body>
-        <button hx-post=""/hello-world"" hx-swap=""outerHTML"" hx-trigger=""click"">Get time</button>
-    </body>
-</html>
-"));
-
-app.MapPost("/hello-world", () => new HtmlResult($@"<div>Hello World {DateTimeOffset.UtcNow}</div>"));
 
 var db = new List<TodoItem>()
 {
@@ -27,19 +15,16 @@ var db = new List<TodoItem>()
 
 app.MapGet("/", () =>
 {
-    var builder = new HtmlContentBuilder();
-    builder.AppendHtml("<body hx-get=\"/todos\" hx-trigger=\"load\" hx-swap=\"innerHTML\">");
-    builder.AppendHtml("</body>");
-    return new HtmlContentResult(Components.Document(builder));
+    return new RazorComponentResult<DocumentComponent>();
 });
 
-app.MapGet("/todos", () => new HtmlContentResult(Components.TodoList(db)));
+app.MapGet("/todos", () => new RazorComponentResult<TodoItemListComponent>(new { Model = db }));
 
 app.MapPost("/todos/{id}/toggle", (string id) =>
 {
     var todo = db.First(t => t.Id.ToString() == id);
     todo.IsCompleted = !todo.IsCompleted;
-    return new HtmlContentResult(Components.TodoItem(todo));
+    return new RazorComponentResult<TodoItemComponent>(new { Model = todo });
 });
 
 app.MapDelete("/todos/{id}", (string id) =>
@@ -51,11 +36,9 @@ app.MapDelete("/todos/{id}", (string id) =>
 
 app.MapPost("/todos", (AddTodoItem command) =>
 {
-    var todo = new TodoItem {Id = Guid.NewGuid(),Name = command.Name, IsCompleted = false };
+    var todo = new TodoItem { Id = Guid.NewGuid(), Name = command.Name, IsCompleted = false };
     db.Add(todo);
-    return new HtmlContentResult(Components.TodoItem(todo));
+    return new RazorComponentResult<TodoItemComponent>(new { Model = todo });
 });
 
 app.Run();
-
-
